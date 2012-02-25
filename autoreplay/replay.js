3830310dffb3ae71a14a9be14e51cb149e0adce2
root <root@ubuntu.(none)>
2012-02-25 00:49:17 -0800

    added icon

--- autoreplay/replay.js
+++ autoreplay/replay.js
@@ -1,4 +1,12 @@
+var addIcon = function(){
+    var iconTag = document.createElement('IMG');
+    iconTag.src = chrome.extension.getURL("icon2.png");
+    iconTag.setAttribute("style","vertical-align:middle;float:right");
+    document.getElementById("watch-actions").appendChild(iconTag);
+};
 var videoObj = function(){
+    addIcon();
+
 	var logman = window.document.getElementById("movie_player");
 	if(logman){
 		setInterval(function(){
