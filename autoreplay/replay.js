38d7fe457095ec8b043e921712fb9b3f681cf7de
Lim Sungguk <limasdf@gmail.com>
2012-04-07 03:53:35 -0700

    commit check

--- autoreplay/replay.js
+++ autoreplay/replay.js
@@ -1,17 +1,29 @@
+var getVideoObj = function(){
+	var flashobj = window.document.getElementById("movie_player");
+    if(flashobj){
+        return flashobj;
+    }
+
+    var html5obj = window.document.getElementsByTagName("video")[0];
+    if(html5obj){
+        return html5obj;
+    }
+};
+
 var addIcon = function(){
     var iconTag = document.createElement('IMG');
     iconTag.src = chrome.extension.getURL("icon2.png");
     iconTag.setAttribute("style","vertical-align:middle;float:right");
     document.getElementById("watch-actions").appendChild(iconTag);
 };
+
 var videoObj = function(){
     addIcon();
-
-	var logman = window.document.getElementById("movie_player");
-	if(logman){
+    var video = getVideoObj();
+	if(video){
 		setInterval(function(){
-			if(logman.getPlayerState() == 0/*unrealize*/){
-				logman.seekTo(0, true);
+			if(video.getPlayerState() == 0/*unrealize*/){
+				video.seekTo(0, true);
 			}	
 		}, 250);
 	}
