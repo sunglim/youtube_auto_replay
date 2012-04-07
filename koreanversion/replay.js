011c430f81e1af29a2791da0f3db1ec061b229b9
Lim Sungguk <limasdf@gmail.com>
2012-04-07 05:19:04 -0700

    changed manifest.json for 1.2 version

--- koreanversion/replay.js
+++ koreanversion/replay.js
@@ -1,22 +1,47 @@
+var getVideoObj = function(){
+    var videoObj;
+	videoObj = window.document.getElementById("movie_player");
+    if(videoObj){
+        return videoObj;
+    }
+
+    videoObj = window.document.getElementsByTagName("video")[0];
+    if(videoObj){
+        videoObj.getPlayerState = function(){
+            return videoObj.ended ? 0 : 1;
+        }
+        videoObj.seekTo = function(value){
+            videoObj.currentTime = value;
+            videoObj.play(); // why play() is necessary?
+        }
+    }
+    
+    return videoObj;
+};
+
 var addIcon = function(){
     var iconTag = document.createElement('IMG');
     iconTag.src = chrome.extension.getURL("icon2.png");
     iconTag.setAttribute("style","vertical-align:middle;float:right");
     document.getElementById("watch-actions").appendChild(iconTag);
 };
-var videoObj = function(){
-    addIcon();
 
-	var logman = window.document.getElementById("movie_player");
-	if(logman){
+var pollingCheckAndSeek = function(){
+    addIcon();
+    var video = getVideoObj();
+	if(video){
 		setInterval(function(){
-			if(logman.getPlayerState() == 0/*unrealize*/){
-				logman.seekTo(0, true);
+			if(video.getPlayerState() == 0/*ended*/){
+				video.seekTo(0, true);
 			}	
 		}, 250);
 	}
 };
 
+var addDebugMenu = function(){
+    var div = document.createElement('div');
+}
+
 setTimeout(function(){
-	videoObj();
+    pollingCheckAndSeek();
 }, 1000);
